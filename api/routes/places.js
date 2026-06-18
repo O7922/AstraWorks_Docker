80090d3482feec7d23758ac7673a7d7105a3c91d
g023c1052 <g023c1052@g.neec.ac.jp>
2026-06-18 14:55:06 +0900

    部分一致検索api実装

--- api/routes/places.js
+++ api/routes/places.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db.js');   // db.js がある場合
+const pool = require('../db.js');
 
 router.get('/', async (req, res) => {
-
     try {
+        let names = req.query.name;
+
+        // パラメータが無い場合
+        if (!names) {
+            const [rows] = await pool.query(
+                'SELECT id, name FROM places'
+            );
+
+            return res.json(rows);
+        }
+
+        // 1件だけの場合は配列化
+        if (!Array.isArray(names)) {
+            names = [names];
+        }
 
         const [rows] = await pool.query(
-            'SELECT id, name FROM places'
+            'SELECT id, name FROM places WHERE name IN (?)',
+            [names]
         );
 
         res.json(rows);
 
     } catch (err) {
-
         console.error('DB ERROR:', err);
 
         res.status(500).json({
